bbf1a0d6166fb66d7fd257136e95bb2ab6f013ca
Martin Jaime <martin.jaime@rootstrap.com>
2020-07-17 16:21:07 -0300

    Initialize apple sign in

---
+++ config/initializers/apple_sign_in.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+AppleSignIn.configure do |config|
+  config.apple_client_id = ENV.fetch('APPLE_CIENT_ID')
+  config.apple_private_key = ENV.fetch('APPLE_PRIVATE_KEY')
+  config.apple_key_id = ENV.fetch('APPLE_KEY_ID')
+  config.apple_team_id = ENV.fetch('APPLE_TEAM_ID')
+  config.redirect_uri = ENV.fetch('APPLE_REDIRECT_URL')
+end
